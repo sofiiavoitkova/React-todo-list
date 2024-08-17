1deaedd765dfb676b1e2a2a88543d098fd467c22
Sofiia <sofiiavoitkova@gmail.com>
2024-08-17 21:31:10 +0200

    chore: remove unnecessary comments from code

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,7 @@ function App(props) {
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
-      // if this task has the same ID as the edited task
       if (id === task.id) {
-        // use object spread to make a new object
-        // whose `completed` prop has been inverted
         return { ...task, completed: !task.completed };
       }
       return task;
